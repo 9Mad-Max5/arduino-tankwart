39344f8078a275174dfde6d3545dd9450a7fa204
9Mad-Max5 <maximilian.huebner95@gmx.de>
2023-05-14 17:18:05 +0200

    first commit

---
+++ arduino-tankwart.ino
@@ -0,0 +1,24 @@
+const int analogPin = A0;    // Analoger Pin für die Spannungsmessung
+const int thresholdLow = 100;  // Schwelle für niedrige Spannung
+const int thresholdHigh = 300; // Schwelle für hohe Spannung
+const int outputPin = 13;     // Ausgangspin zum Schalten
+
+void setup() {
+  pinMode(outputPin, OUTPUT);  // Output-Pin festlegen
+  Serial.begin(115200);          // Serielle Kommunikation starten
+}
+
+void loop() {
+  int sensorValue = analogRead(analogPin);  // Spannungswert lesen
+  Serial.write("Got value: ");
+  Serial.println(sensorValue);
+  
+
+  if (sensorValue < thresholdLow) {
+    digitalWrite(outputPin, LOW);   // Ausgang ausschalten
+  } else if (sensorValue > thresholdHigh) {
+    digitalWrite(outputPin, HIGH);  // Ausgang einschalten
+  }
+
+  delay(1000);  // 5 Sekunden warten
+}
