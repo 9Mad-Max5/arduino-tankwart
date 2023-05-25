52fd937b5be2876e304a01e523a4409bb6305b73
Maximilian <3002909@stud-hs.mannheim.de>
2023-05-26 01:53:25 +0200

    fast final

--- arduino-tankwart.ino
+++ arduino-tankwart.ino
@@ -1,15 +1,22 @@
 const int analogPin = A0;    // Analoger Pin für die Spannungsmessung
 const int thresholdLow = 60;  // Schwelle für niedrige Spannung
-const int thresholdHigh = 200; // Schwelle für hohe Spannung
+const int thresholdHigh = 220; // Schwelle für hohe Spannung
 const int outputPin = 13;     // Ausgangspin zum Schalten
 
 const int lowerSensorPin = 2;     // Pin für den unteren Sensor (Keine Flüssigkeit)
 const int upperSensorPin = 3;     // Pin für den oberen Sensor (Flüssigkeit im Tank)
 
-const int maxOnTime = 12000;      // Maximale Einschaltdauer in Millisekunden (12 Sekunden)
-const int cooldownTime = 60000;   // Cool-Down-Zeit in Millisekunden (60 Sekunden)
+int on_counter = 0;
+const int max_on_cycle = 12;      // Maximale Einschaltdauer in Millisekunden (12 Sekunden)
+const int cool_down_cycle = 60;   // Cool-Down-Zeit in Millisekunden (60 Sekunden)
+
+const unsigned long maxOnTime = 12000;      // Maximale Einschaltdauer in Millisekunden (12 Sekunden)
+const unsigned long cooldownTime = 60000;   // Cool-Down-Zeit in Millisekunden (60 Sekunden)
+// const int cooldownTime = 6000;   // Cool-Down-Zeit in Millisekunden (60 Sekunden)
 
 unsigned long lastActivationTime = 0;  // Zeitpunkt der letzten Aktivierung des Ausgangs
+unsigned long cooldown_start = 0;  // Zeitpunkt der letzten Aktivierung des Cooldowns
+
 bool inCooldown = false;                // Gibt an, ob sich der Ausgang im Cool-Down-Zeitraum befindet
 
 void setup() {
@@ -17,6 +24,7 @@ void setup() {
   pinMode(lowerSensorPin, INPUT);      // Eingangs-Pin für den unteren Sensor festlegen
   pinMode(upperSensorPin, INPUT);      // Eingangs-Pin für den oberen Sensor festlegen
   Serial.begin(115200);          // Serielle Kommunikation starten
+  deactivateOutput();
 }
 
 void loop() {
@@ -34,11 +42,17 @@ void loop() {
   Wir verkraften ist das es leer ist in diesem Szenario deswegen warten auf ein HIGH wobei dieser Fall eher unkritisch ist
   Da es schlimmer ist wenn es überläuft wie das es nicht pumpt nehmen wir bei Abriss == Voll
   */
-
-  if ((sensorValue < thresholdLow && !isOutputActive() && !inCooldown) || (lowerSensorValue == HIGH && !isOutputActive() && !inCooldown)) {
-    activateOutput();
+  if (inCooldown){
+    colldown_managment();
+  } else if (upperSensorValue == HIGH && !inCooldown && sensorValue < thresholdHigh){
+    if ((sensorValue < thresholdLow && !isOutputActive()) || (lowerSensorValue == HIGH && !isOutputActive())) {
+      activateOutput();
+  }
   } else if (sensorValue > thresholdHigh || upperSensorValue == LOW) {
     deactivateOutput();
+  }
+  if (isOutputActive() && !inCooldown){
+    check_runtime();
   }
 
   delay(1000);  // Kurze Verzögerung zur Entprellung des Sensors
@@ -48,20 +62,38 @@ bool isOutputActive() {
   return (digitalRead(outputPin) == LOW);
 }
 
+void check_runtime(){
+  unsigned long currentTime = millis();
+  if (currentTime - lastActivationTime >= maxOnTime) {
+    Serial.println("Maximum on time reached");
+    inCooldown = true;
+    cooldown_start = currentTime;
+    deactivateOutput();
+    colldown_managment();
+  }
+}
+
+void colldown_managment(){
+  unsigned long currentTime = millis();
+  unsigned long passedtime = currentTime - cooldown_start;
+  if (passedtime >= cooldownTime) {
+    Serial.println("Cooldown finished");
+    inCooldown = false;
+    cooldown_start = 0;
+  }
+  else{
+    Serial.write("Cooldown time expired: ");
+    Serial.println(passedtime);
+  }
+}
+
 void activateOutput() {
   unsigned long currentTime = millis();
-  
-  // Überprüfen, ob der Cool-Down-Zeitraum abgelaufen ist
-  if (currentTime - lastActivationTime >= cooldownTime) {
-    digitalWrite(outputPin, LOW);    // Ausgang einschalten
-    lastActivationTime = currentTime; // Zeitpunkt der Aktivierung aktualisieren
-    delay(maxOnTime);                  // Maximale Einschaltdauer abwarten
-    digitalWrite(outputPin, HIGH);    // Ausgang ausschalten (umgekehrte Logik)
-    inCooldown = true;                 // Cool-Down-Modus aktivieren
-  }
+  digitalWrite(outputPin, LOW);    // Ausgang einschalten
+  lastActivationTime = currentTime; // Zeitpunkt der Aktivierung aktualisieren
 }
 
 void deactivateOutput() {
   digitalWrite(outputPin, HIGH);   // Ausgang ausschalten (umgekehrte Logik)
-  inCooldown = false;               // Cool-Down-Modus deaktivieren
+  lastActivationTime = 0;
 }
